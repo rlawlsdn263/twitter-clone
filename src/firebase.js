9dafa7e187c7367908b955088da2cd7a55ac0949
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-25 14:23:39 +0900

    [2] #1.0 React + Firebase Setup
    - CRA로 리액트앱 빌드
    - firebase.js 파일 생성

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOuU0Yh-cvwKAqpCtXdXD_jXp19ZfW7qU",
+  authDomain: "twitter-clone-a40f2.firebaseapp.com",
+  projectId: "twitter-clone-a40f2",
+  storageBucket: "twitter-clone-a40f2.appspot.com",
+  messagingSenderId: "207368094266",
+  appId: "1:207368094266:web:0d5bfaac28788d34ad6b03"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

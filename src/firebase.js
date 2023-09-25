1693cb6ab5e58e8d61513004410e7d0a2c7d0fba
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-25 14:24:24 +0900

    [2] #1.1 Securing the Keys
    - .env 파일 생성
    - .gitignore 수정

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOuU0Yh-cvwKAqpCtXdXD_jXp19ZfW7qU",
-  authDomain: "twitter-clone-a40f2.firebaseapp.com",
-  projectId: "twitter-clone-a40f2",
-  storageBucket: "twitter-clone-a40f2.appspot.com",
-  messagingSenderId: "207368094266",
-  appId: "1:207368094266:web:0d5bfaac28788d34ad6b03"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase

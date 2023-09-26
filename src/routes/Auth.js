4ba510f02cdb0b5226860d68a49c1ea522a7659a
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-26 11:50:18 +0900

    [3] #2.5. Social Login
    - signInWithPopup은 팝업창을 띄워 써드파티 로그인을 하게 해주는 함수
    - button의 name 속성을 사용해 거기에 알맞는 Provider 제공
    - GoogleAuthProvider와 GithubAuthProvider를 사용해 인스턴스를 생성
    - signInWithPopup을 async & await으로 처리

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { authService } from 'myFirebase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const Auth = () => {
 
@@ -20,18 +20,14 @@ const Auth = () => {
     }
   }
 
-  // createUserWithEmailAndPassword와 signInWithEmailAndPassword은 Promise를 반환하기 때문에 asyn&await을 사용
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    // async&await은 try... catch와 보통 같이 사용된다
     try {
       let data;
       if(newAccount) {
-        // 새로운 계정 생성 + 자동 로그인
         data = await createUserWithEmailAndPassword(authService, email, password);
       } else {
-        // 로그인
         data = await signInWithEmailAndPassword(authService, email, password);
       }
       console.log(data);
@@ -44,6 +40,26 @@ const Auth = () => {
     setNewAccount((prev) => !prev);
   }
 
+  // signInWithPopup은 Promise이기 때문에 async & await가 사용가능
+  const onSocialClick = async (event) => {
+    // name 속성으로 구분
+    const { target: { name } } = event;
+
+    // 써드파티 로그인은 Provider를 제공해야함
+    let provider;
+
+    if(name === "google") {
+      // 구글 로그인일 때 GoogleAuthProvider 인스턴스 생성
+      provider = new GoogleAuthProvider();
+    } else if(name === "github") {
+      // 깃헙 로그인일 때 GithubAuthProvider 인스턴스 생성
+      provider = new GithubAuthProvider();
+    }
+
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
+  }
+
   return (
       <div>
         <form onSubmit={onSubmit}>
@@ -51,13 +67,15 @@ const Auth = () => {
           <input type='password' name='password' placeholder='Password' required value={password} onChange={onChange}/>
           <input type='submit' value={ newAccount ? "Create Account" : "Log In" } />
 
-          {/* 에러 메세지 출력 */}
           { error }
+          
         </form>
         <span onClick={toggleAccount}>{ newAccount ? "Sign In"  : "Create Account" }</span>
         <div>
-          <button>Continue with Google</button>
-          <button>Continue with Github</button>
+
+          {/* name 속성 추가 */}
+          <button name="google" onClick={onSocialClick}>Continue with Google</button>
+          <button name="github" onClick={onSocialClick}>Continue with Github</button>
         </div>
       </div>
     )  

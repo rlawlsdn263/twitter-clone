5a7ed866d0b36c5fa08512daf45134336f35658a
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-26 12:17:37 +0900

    [3] #2.6 Log Out
    - Navgation 컴포넌트 생성
    - signOut으로 로그아웃 기능 구현
    - useNavigate로 리디렉션 기능 구현

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,29 @@
-const Profile = () => <span>Profile</span>;
+import { signOut } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+import { authService } from 'myFirebase';
+
+const Profile = () => {
+  // 리디렉션
+  let navigate = useNavigate();
+
+  const onLogOutClick = async () => {
+    try {
+      // 로그아웃 기능
+      const data = await signOut(authService);
+      console.log(data);
+      
+      // 홈으로 리디렉션 
+      navigate('/');
+    } catch(err) {
+      console.log(err.message);
+    }
+  }
+
+  return (
+    <>
+    <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  )
+}
 
 export default Profile;

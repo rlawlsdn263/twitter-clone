0c3474acc3e9c3d7e91db058bc4e5c0d48cb5203
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-26 10:03:36 +0900

    [3] #2.1 Login Form part One
    - state를 만들어 email, password의 값을 각각 저장
    - onChange 핸들러를 만들어 event.target의 name에 따라 어떤 state에 값이 저장되는 지를 결정
    - onSubmit 핸들러를 만들어 form 태그의 새로고침을 방지
    - 구조분해할당으로 name, value 변수 생성 및 값 할당

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,47 @@
-const Auth = () => <span>Auth</span>;
+import { useState } from 'react';
+
+const Auth = () => {
+
+  // 이메일과 비밀번호를 저장할 state를 생성
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // input 안의 들어갈 onChange 이벤트
+  // event.target의 name을 통해 어느 state에 값을 저장할지 결정
+  const onChange = (event) => {
+    // const name = event.target.name;
+    // const value = event.target.value;
+    // const { name, value } = event.target;
+    // 구조분해할당으로 event.target.name과 event.target.value를 풀어서 변수에 각각 할당함
+    const { target: { name, value } } = event;
+
+    console.log(name, value);
+    if(name === 'email') {
+      setEmail(value);
+    } else if(name === 'password') {
+      setPassword(value);
+    }
+  }
+
+  // form의 onSubmit 이벤트로 주어지는 함수
+  // form으로 인한 새로고침을 방지하기 위한 함수
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input type='text' name='email' placeholder='Email' required value={email} onChange={onChange}/>
+          <input type='password' name='password' placeholder='Password' required value={password} onChange={onChange}/>
+          <input type='submit' value="Log In" />
+        </form>
+        <div>
+          <button>Continue with Google</button>
+          <button>Continue with Github</button>
+        </div>
+      </div>
+    )  
+}
 
 export default Auth;

d36542e8126535d6d864610203aa824420b76933
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-27 13:46:32 +0900

    [4] #3.2 Getting the Nweets
    - getDocs로 컬렉션 호출
    - setTweets에 가져온 배열을 넣어주고 화면에 렌더링함
    
    getDocs를 사용해 컬렉션에서 트윗이 담긴 문서를 가져올 수 있다. 하지만 forEach 내부에서 setTweets()을 실행하자 배열의 요소마다 setTweets가 실행되며 또한 리액트 엄격모드로 인해 렌더링이 두 번 일어나 총 6개의 값이 추가되는 버그가 발생했다.
    
    이를 해결하기 위해 getDocs로 가져온 값 안의 docs 속성에 접근해 map을 사용하여 새로운 배열을 반환했다. 그리고 이 값을 setTweets에 넣어줌으로 엄격모드로 인해 useEffect가 재실행되더라도 배열  안의 값이 불필요하게 늘어나지 않게 됐다.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,56 @@
-import { useState } from 'react';
-import { collection, addDoc } from "firebase/firestore"; 
+import { useState, useEffect } from 'react';
+import { collection, addDoc, getDocs  } from "firebase/firestore"; 
 import { dbService } from 'myFirebase';
 
 const Home = () => {
 
   const [tweet, setTweet] = useState('');
 
-  // 새로고침 방지용 함수
+  // tweets state에는 DB에 저장된 데이터가 들어온다
+  const [tweets, setTweets] = useState([]);
+
+  // 매번 setTweets()가 실행되기 때문에 불필요한 추가가 너무 많이 발생한다.
+  // 매번 하는 것보다 한 번만 하는 게 좋다.
+
+  /* 
+    const getTweets = async () => {
+
+      // getDocs로 DB에 저장된 트윗을 가져옴
+      const dbTweets = await getDocs(collection(dbService, "tweets"));
+      
+      dbTweets.forEach((doc) => {
+        
+        // doc 안의 든 트윗들을 배열에 추가해줌
+        const tweetObject = {
+          // 구조분해할당으로 doc.data()의 모든 걸 풀어서 추가한다
+          ...doc.data(),
+          id: doc.id
+        }
+        setTweets(prev => [tweetObject, ...prev])
+      });
+    } 
+  */
+
+  const getTweets = async () => {
+    // getDocs로 DB에 저장된 트윗을 가져옴
+    const dbTweets = await getDocs(collection(dbService, "tweets"));
+
+    // docs를 map으로 순환해 새로운 배열을 만듦
+    const newTweets = dbTweets.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }));
+
+    // 만든 배열을 setTweets에 넣음으로 업데이트함
+    setTweets(newTweets);
+}
+
+
+  // useEffect 안에서 getTweets()를 실행해 트윗을 가져옴
+  useEffect(() => {
+    getTweets();
+  }, [])
+
   const onSubmit = async (event) => {
     event.preventDefault();
     
@@ -23,7 +67,6 @@ const Home = () => {
     setTweet('');
   }
 
-  // state와 연동된 onChange 핸들러
   const onChange = (event) => {
     const { target: { value } } = event;
 
@@ -36,6 +79,16 @@ const Home = () => {
         <input type="text" value={tweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Tweet" />
       </form>
+
+      <div>
+        {
+          tweets.map(tweet => (
+            <div key={tweet.id}>
+              <h4>{tweet.tweet}</h4>
+            </div>  
+          ))
+        }
+      </div>
     </div>
   )
 }

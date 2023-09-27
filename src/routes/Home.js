9f8bfe7990312296f1142b1a29062fc7c86c3cc9
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-27 13:04:42 +0900

    [4] #3.1 Nweeting!
    - getFireStore()로 파이어스토어 인스턴스 생성
    - addDoc으로 컬렉션 생성 및 다큐먼트 추가 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,26 @@
 import { useState } from 'react';
+import { collection, addDoc } from "firebase/firestore"; 
+import { dbService } from 'myFirebase';
 
 const Home = () => {
 
   const [tweet, setTweet] = useState('');
 
   // 새로고침 방지용 함수
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    
+    // addDoc을 사용해 컬렉션을 만들고 그 안에 문서를 추가함
+    try {
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    setTweet('');
   }
 
   // state와 연동된 onChange 핸들러

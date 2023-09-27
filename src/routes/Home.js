34e2cd84b7c8dafd80a1754c4c0c6bf8550c9d30
rlawlsdn263 <rlawlsdn263@naver.com>
2023-09-27 12:21:44 +0900

    [4] #3.0 Form and Database Setup
    - Home 컴포넌트 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,29 @@
-const Home = () => <span>Home</span>;
+import { useState } from 'react';
+
+const Home = () => {
+
+  const [tweet, setTweet] = useState('');
+
+  // 새로고침 방지용 함수
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  // state와 연동된 onChange 핸들러
+  const onChange = (event) => {
+    const { target: { value } } = event;
+
+    setTweet(value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={tweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  )
+}
 
 export default Home;

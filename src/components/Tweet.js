3e48af3998c4606b70609e67f8f3583297f6cd5b
rlawlsdn263 <rlawlsdn263@naver.com>
2023-10-04 11:35:25 +0900

    [4] #3.5 Delete and Update part Two
    - deleteDoc을 사용해 트윗 삭제 기능 추가
    - updateDoc을 사용해 트윗 업데이트 기능 추가
    
    editing state를 만들고 초깃값으로 불린값을 전달했다. editing state의 값에 따라 트윗이 업데이트 모드로 변경되거나 원래로 돌아오게 하기 위함이다.
    
    newTweet state에는 에디팅 모드일 때 input에 입력되는 새로운 트윗의 값이 저장된다.
    
    toggleEditing 함수는 editing state의 불린형값을 토글링해 에디팅 모드를 활성화하거나 닫는 용도다.
    
    onSubmit은 업데이트르 위한 함수로 preventDefault()로 form의 초기화 기능을 방지하고, updateDoc()을 사용해 기존의 트윗을 newTweet에 저장된 값으로 업데이트 한다. 또한, 업데이트가 종료되면 setEditing을 사용해 에딧모드를 종료한다.
    
    onChange 함수에는 newTweet의 값을 업데이트 하기 위해 사용된다.
    
    onDeleteClick은 confirm()을 사용해 윈도우창을 보여준 다음, 사용자가 true를 선택했을 경우 deleteDoc()을 활용해 해당 트윗을 삭제한다.

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,78 @@
+import { useState } from 'react';
+import { doc, deleteDoc, updateDoc  } from "firebase/firestore";
+import { dbService } from 'myFirebase';
+
 const Tweet = ({ tweetObj, isOwner }) => {
+
+  // editing state는 edit 버튼의 클릭 유무를 관리함 - 에디팅 모드임을 파악함
+  const [editing, setEditing] = useState(false);
+
+  // newTweet state는 tweet의 업데이트될 텍스트를 위한 state임
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+  // editing state의 상태를 토글링하는 함수
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onSubmit = async (event) => {
+    // form 초기화 방지
+    event.preventDefault();
+
+    // 트윗 update 함수
+    await updateDoc(doc(dbService, "tweets", tweetObj.id), {
+      text: newTweet
+    });
+
+    // 에디팅 모드 종료
+    setEditing(false);
+  }
+
+  // newTweet 업데이트 함수
+  const onChange = (event) => {
+    const {target: { value }} = event;
+    setNewTweet(value);
+  }
+
+  const onDeleteClick = async () => {
+    // confirm은 윈도우 알림창을 띄어주고 해당 값의 true, false 값을 반환한다
+    const ok = window.confirm("Are you sure you want to delete this newwet?");
+    if(ok) {
+      // deleteDoc()을 사용해 tweets 컬렉션 안의 tweetObj.id를 가진 문서를 삭제함
+      await deleteDoc(doc(dbService, "tweets", tweetObj.id));
+    }
+  }
+
   return (
     <div>
-      <h4>{tweetObj.text}</h4>
       {
         // 트윗의 작성자만 트윗을 삭제, 수정할 수 있음
-        isOwner && (
-          <>
-              <button>Delete Tweet</button>
-              <button>Edit Tweet</button>   
+        // editing state가 true면 input을 보여주고, false면 Tweet 컴포넌트를 렌더링함
+        editing ? (
+          <>          
+            <form onSubmit={onSubmit}>
+              <input 
+                type='text' 
+                placeholder='Edit your tweet' 
+                value={newTweet} 
+                required 
+                onChange={onChange}  
+              />
+              <input type="submit" value="Update Tweet" />
+            </form>
+            <button onClick={toggleEditing}>Cancel</button>
+          </>
+        ) :
+        (
+          <>          
+            <h4>{tweetObj.text}</h4>
+            {
+              // isOwner가 true면 삭제, 수정 버튼을 렌더링함
+              isOwner && (
+                <>
+                    <button onClick={onDeleteClick}>Delete Tweet</button>
+                    <button onClick={toggleEditing}>Edit Tweet</button>   
+                </>
+              )
+            }
           </>
         )
       }
